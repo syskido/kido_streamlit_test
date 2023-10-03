30a390e216b086bce56caf09d5c704fd3ff7249e
kido <61651321+syskido@users.noreply.github.com>
2023-10-03 13:32:37 +0900

    Add files via upload

---
+++ 2_📶_Chat_Demo.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+
+
+st.markdown("# Mapping Demo")
+st.sidebar.header("Mopping Demo")
+st.write(
+    
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+)
+
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st  
+import pandas as pd        
+import numpy as np       
+
+from time import sleep  
+
+#페이지 기본 설정
+st.set_page_config(
+    page_icon="*️⃣",
+    page_title="나령 윤기도 스트림릿 배포하기",
+    layout="wide",
+)
+
+
+# 페이지 헤더, 서브헤더 제목 설정
+st.header("나령 윤기도 페이지에 오신것을 환영합니다.🤗🤗")
+st.subheader("스트림릿 기능 맛보기")
+
+# 페이지 컬럼 분할(예: 부트스트랩 컬럼, 그리드)
+cols = st.columns((1,1,2))
+cols[0].metric("10/11", "15 ℃", "2")
+cols[0].metric("10/12", "17 ℃", "2 ℉")
+cols[0].metric("10/13", "15 ℃", "2")
+cols[1].metric("10/14", "17 ℃", "2 ℉")
+cols[1].metric("10/15", "14 ℃", "-3 ℉")
+cols[1].metric("10/16", "13 ℃", "-1 ℉")
+
+#라인 그래프 데이터 생성(with. Pandas)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+#컬럼 나머지 부분에 라인차트 생성
+cols[2].line_chart(chart_data)
